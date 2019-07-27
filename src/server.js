fee1c9844dfeeb97346875164e012a958fa6059c
akexinad <danixeka@gmail.com>
2019-07-28 08:05:45 +1000

    38. Serving HTML Pages via Express

--- src/server.js
+++ src/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('HELLO WORLD');
+
+    const pathToHtmlFile = path.resolve(__dirname, '../dist/index.html');
+
+    // In order to read the content of the html file, node requires utf-8 unicode format.
+    const content = fs.readFileSync(pathToHtmlFile, 'utf-8');
+
+    res.send(content);
+
 });
 
 app.listen(3000, () => {
+
     console.log(`Listening on http://localhost:3000/`);
-})
+
+});
